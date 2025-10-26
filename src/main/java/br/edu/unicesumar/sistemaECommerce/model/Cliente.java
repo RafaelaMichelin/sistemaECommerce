496def1149db0736ccdd3300424ecf8c44f7d46a
Rafaela Michelin <RafaelaMichelin@gmail.com>
2025-10-26 11:28:58 -0300

    - Configurando o javafx e adicionando primeiras colunas
    Coluna na classe Cliente, ID e Nome

--- src/main/java/br/edu/unicesumar/sistemaECommerce/model/Cliente.java
+++ src/main/java/br/edu/unicesumar/sistemaECommerce/model/Cliente.java
@@ -1,5 +1,25 @@
 package br.edu.unicesumar.sistemaECommerce.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+//INFORMAR PARA O JPA QUE ESTE OBJETO É UMA TABELA NO BD
+@Entity
+@Table(name="cliente_tbl")
+
 public class Cliente {
 
+    @Id 
+    @GeneratedValue(strategy = GenerationType.AUTO )
+	@Column(name="id_cliente")
+    private int id;
+
+    @Column(name="nome_cliente")
+    private String nome;
+
+    
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,21 @@
 module br.edu.unicesumar {
+    // Requisitos JavaFX
     requires javafx.controls;
     requires javafx.fxml;
     requires javafx.media;
+    
+    // REQUISITOS JPA / HIBERNATE (PARA RESOLVER O ERRO)
+    // 1. Módulo JPA API (fornecido pela dependência jakarta.persistence-api)
+    requires jakarta.persistence; 
+    
+    // 2. Módulo Hibernate Core (fornecido pela dependência hibernate-core)
+    requires org.hibernate.orm.core; 
+
+    // 3. Módulo do MariaDB (Obrigatório para drivers JDBC)
+    requires java.sql; 
+    requires org.mariadb.jdbc; 
+    
+    // Permissões
     opens br.edu.unicesumar.sistemaECommerce to javafx.fxml;
     exports br.edu.unicesumar.sistemaECommerce;
 }
