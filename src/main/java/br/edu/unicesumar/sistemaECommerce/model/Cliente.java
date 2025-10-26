bb143d3ec521b70672f2a9cff12ce8286f5b3a58
Rafaela Michelin <RafaelaMichelin@gmail.com>
2025-10-26 12:21:34 -0300

    -Criando getters e setters p/Cliente

--- src/main/java/br/edu/unicesumar/sistemaECommerce/model/Cliente.java
+++ src/main/java/br/edu/unicesumar/sistemaECommerce/model/Cliente.java
@@ -33,5 +33,56 @@ public class Cliente {
     @Embedded
     private Endereco endereco;
 
+
+    //CONSTRUCTOR 
+    public Cliente(){}
+
+    public Cliente(int id, String nome, String cpf, String email, Endereco endereco){
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.endereco = endereco;
+    }
+
+    public int getId(){
+        return this.id;
+    }
+
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getCpf(){
+        return this.cpf;
+    }
+    public void setCpf(String cpf){
+        this.cpf = cpf;
+    }
+
+    public String getEmail(){
+        return this.email;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+    
+    
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
     
 }
