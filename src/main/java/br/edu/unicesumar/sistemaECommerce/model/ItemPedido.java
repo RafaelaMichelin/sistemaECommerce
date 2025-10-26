c3a7c0fe40319c176661b9ea2f39fee160e8076a
Rafaela Michelin <RafaelaMichelin@gmail.com>
2025-10-26 18:54:36 -0300

    -Criando ItemPedido

---
+++ src/main/java/br/edu/unicesumar/sistemaECommerce/model/ItemPedido.java
@@ -0,0 +1,53 @@
+package br.edu.unicesumar.sistemaECommerce.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table; 
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn; 
+
+@Entity
+@Table(name="itempedido_tbl")
+
+public class ItemPedido {
+    
+    @Id 
+    @GeneratedValue(strategy = GenerationType.AUTO )
+
+	@ManyToOne
+    @JoinColumn(name = "id_produto")
+    private Produto produto;
+
+    @Column(name="quantiade_pedido")
+     private int quantidade;
+
+
+     //CONSTRUCTOR 
+
+     public ItemPedido(){}
+
+     public ItemPedido(Produto protudo, int quantidade){
+        this.produto = produto;
+        this.quantidade = quantidade;
+     }
+
+       public Produto getProduto() {
+        return produto;
+    }
+
+    public void setProduto(Produto produto) {
+        this.produto = produto;
+    }
+
+     public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+}
