7905fdfc04489d660d17309b352ea095a627e9f1
Rafaela Michelin <RafaelaMichelin@gmail.com>
2025-10-27 20:27:28 -0300

    -Adicionando itens nas colunas do banco de dados

--- src/main/java/br/edu/unicesumar/sistemaECommerce/MainApp.java
+++ src/main/java/br/edu/unicesumar/sistemaECommerce/MainApp.java
@@ -2,12 +2,14 @@ package br.edu.unicesumar.sistemaECommerce;
 
 import br.edu.unicesumar.sistemaECommerce.DAO.CategoriaDAO;
 import br.edu.unicesumar.sistemaECommerce.DAO.ProdutoDAO;
+import br.edu.unicesumar.sistemaECommerce.DAO.ClienteDAO;
+import br.edu.unicesumar.sistemaECommerce.DAO.CarrinhoDAO;
 import br.edu.unicesumar.sistemaECommerce.DAO.DAO;
 import br.edu.unicesumar.sistemaECommerce.model.Categoria;
 import br.edu.unicesumar.sistemaECommerce.model.Produto;
 import br.edu.unicesumar.sistemaECommerce.model.Cliente;
 import br.edu.unicesumar.sistemaECommerce.model.Pedido;
-
+import br.edu.unicesumar.sistemaECommerce.model.Carrinho;
 
 public class MainApp {
     public static void main(String[] args) {
@@ -17,27 +19,33 @@ public class MainApp {
         try {
             // Criando uma categoria
             Categoria categoria = new Categoria();
-            categoria.setNome("Eletrônicos");
+            categoria.setNome("Papelaria");
+          
 
             // Criando um produto
             Produto produto = new Produto();
-            produto.setNome("Notebook ");
-            produto.setPreco(4999.99);
+            produto.setNome("Lápis ");
+            produto.setDescricao("Lápis de cor");
+            produto.setPreco(10.00);
+
+            //Criando um Cliente 
+            Cliente cliente = new Cliente();
+            cliente.setNome("Renan");
+            cliente.setEmail("Renan@gmail.com");
            
+
 
             // Instanciando DAOs
             CategoriaDAO categoriaDAO = new CategoriaDAO();
             ProdutoDAO produtoDAO = new ProdutoDAO();
+            ClienteDAO clienteDAO = new ClienteDAO();
 
             // Salvando no banco
             categoriaDAO.save(categoria);
             produtoDAO.save(produto);
+            clienteDAO.save(cliente);
 
-            // Listando produtos
-            System.out.println("\nProdutos cadastrados:");
-            for (Produto p : produtoDAO.listAllProdutos()) {
-                System.out.println("- " + p.getNome() + " | R$ " + p.getPreco());
-            }
+          
 
             System.out.println("\n Conexão e persistência bem-sucedidas!");
         } catch (Exception e) {
